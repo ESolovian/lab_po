f3a46d043d877b24e1c6d9a8cbf7cedd09383232
ESolovian <80752492+ESolovian@users.noreply.github.com>
2022-12-22 14:09:27 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+#include <cassert>
+
+int calc(int a, char oper, int b){
+    if (oper == '+'){
+        return a + b;
+    } else if (oper == '-'){
+        return a - b;
+    } else if (oper == '*'){
+        return a * b;
+    } else if (oper == '/'){
+        return a / b;
+    } else if (oper == '^'){
+        return std::pow(a, b);
+    }
+
+    return 0;
+}
+
+void testCalc(){
+    assert(calc(23, '+', 268) == 291);
+    assert(calc(173, '-', 745) == -572);
+    assert(calc(29, '*', 17) == 493);
+    assert(calc(188, '/', 4) == 47);
+    assert(calc(2, '^', 10) == 1024);
+    std::cout << "Program is working!" << '\n';
+}
+
+int main() {
+    testCalc();
+    return 0;
+}
